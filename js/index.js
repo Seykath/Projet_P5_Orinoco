a59aa8aa330345af70042f3985837729d2020203
Tony <tony.roussarie@gmail.com>
2020-08-10 14:58:38 +0200

    modif responsive

--- js/index.js
+++ js/index.js
@@ -3,13 +3,12 @@ const cameraList = document.getElementById('camera__list');
 let url = "http://localhost:3000/api/cameras";
 
 
-
-////////////////////////////////////////////
-
+// function qui récupère les caméra via l'url et ensuite affiche le resultat sous format json
 async function getCamera(url) {
     let result = await fetch(url)
     return result.json()
 }
+
 
 
 getCamera(url).then(cameras => {
@@ -30,66 +29,5 @@ getCamera(url).then(cameras => {
                             </div >\
                     </div > `
     });
-
-
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetch(url)
-//     .then(
-//         getCamera
-//     )
-//     .catch(
-//         getError
-//     )
-
-
-// function getCamera(reponse) {
-//     if (reponse.status !== 200) {
-//         console.log('il y\'a un problème' + reponse.status);
-//         return;
-//     }
-//     reponse.json()
-//         .then(function (data) {
-//             console.log(data);
-//             data.forEach(camera => {
-//                 console.log(camera);
-//                 cameraList.innerHTML += '<div class="col-lg-4 col-md-6 mb-4">\
-//                         <div class="card w-80" >\
-//                                 <a href="#"><img class="card-img-top" src="'+ camera.imageUrl + '"></a>\
-//                                 <div class="card-body">\
-//                                     <h3 class="card-title">\
-//                                         <a href="produit.html?id='+ camera._id + '">' + camera.name + '</a>\
-//                                     </h3>\
-//                                     <h4 class="price">'+ camera.price + ' €' + '</h4>\
-//                                     <p class="card-text">' + camera.description + '</p>\
-//                                 </div>\
-//                             </div>\
-//                     </div >'
-//             });
-//         })
-
-// }
-
-
-// function getError() {
-//     console.log('erreur')
-// }
