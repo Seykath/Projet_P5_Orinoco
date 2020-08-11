44961baff6d748b0ff330253cac0f8e2d2dee41e
Tony <tony.roussarie@gmail.com>
2020-08-11 15:21:53 +0200

    correctifs

--- controllers/camera.js
+++ controllers/camera.js
@@ -48,12 +48,12 @@ exports.getOneCamera = (req, res, next) => {
  */
 exports.orderCameras = (req, res, next) => {
   if (!req.body.contact ||
-      !req.body.contact.firstName ||
-      !req.body.contact.lastName ||
-      !req.body.contact.address ||
-      !req.body.contact.city ||
-      !req.body.contact.email ||
-      !req.body.products) {
+    !req.body.contact.firstName ||
+    !req.body.contact.lastName ||
+    !req.body.contact.address ||
+    !req.body.contact.city ||
+    !req.body.contact.email ||
+    !req.body.products) {
     return res.status(400).send(new Error('Bad request!'));
   }
   let queries = [];
--- js/index.js
+++ js/index.js
@@ -17,14 +17,14 @@ getCamera(url).then(cameras => {
         console.log(camera)
         cameraList.innerHTML += `<div class="col-lg-4 col-md-6 mb-4">\
                         <div class="card" >\
-                                <a href="products.html?id=${camera._id}"><img class="card-img-top" src="${camera.imageUrl}"></a>\
+                                <a href="/products.html?id=${camera._id}"><img class="card-img-top" src="${camera.imageUrl}"></a>\
                                 <div class="card-body text-center">\
                                     <h3 class="card-title">\
-                                        <a href="products.html?id=${camera._id}">${camera.name}</a>\
+                                        <a href="/products.html?id=${camera._id}">${camera.name}</a>\
                                     </h3>\
                                     <h4 class="price">${camera.price / 100},00 €</h4>\
                                     <p class="card-text">${camera.description}</p>\
-                                    <a href="products.html?id=${camera._id}"><button type"button" class="btn btn-success">Détails</button></a>\
+                                    <a href="/products.html?id=${camera._id}"><button type"button" class="btn btn-success">Détails</button></a>\
                                 </div >\
                             </div >\
                     </div > `
